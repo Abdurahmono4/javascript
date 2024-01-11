c12771e77936a2a5f7bc880371b449e3a4cf6a0f
Abdurahmono4 <olimjonovrahmonjon7@gmail.com>
2024-01-12 02:56:53 +0500

    new js solutions

---
+++ app.js
@@ -0,0 +1,24 @@
+// 1-masala
+// let numb1 = +prompt(eni);
+// let numb2 = +prompt(boyi);
+// let numb3 = +prompt(balandligi);
+// let result = eni * boyi * balandligi;
+// console.log(result);
+
+// 2-masala
+//let numb = cub;
+//let result = +prompt(cub ** 3);
+//console.log(result);
+
+// 3-masala
+//let numb1 = +prompt(son1);
+//let numb2 = +prompt(son2);
+//let result =((numb1+numb2)**2)
+//console.log(result)
+
+// 4-masala
+
+// 7-masala
+//let numb1 = +prompt(son1);
+//let result =((numb1-10)**3)
+//console.log(result)
